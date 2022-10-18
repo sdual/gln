703b6b67a1f3a23e031934ec9a431715047b3836
sdual <shr.nrht.0623@gmail.com>
2022-10-18 20:21:58 +0900

    implement half space context

--- src/model/context_func.rs
+++ src/model/context_func.rs
@@ -1,19 +1,74 @@
+use std::convert::TryInto;
+
+use ndarray::Array;
+use ndarray::prelude::*;
+use rand::thread_rng;
+use rand_distr::{Distribution, Normal};
+
 pub trait ContextFunction {
-    fn indicator_func(&self, side_effects: &Vec<f32>) -> Vec<bool>;
+    fn indicator_func(&self, side_info: &[f32]) -> Vec<bool>;
 }
 
-pub struct HalfSpaceContext {}
+pub struct HalfSpaceContext {
+    feature_dim: usize,
+    context_dim: usize,
+    context_maps: Vec<Vec<f32>>,
+    context_bias: Vec<f32>,
+}
+
+impl HalfSpaceContext {
+    pub fn new(context_dim: usize, feature_dim: usize) -> Self {
+        let normal = Normal::new(0.0, 1.0).unwrap();
+        let context_maps: Vec<Vec<f32>> = (0..context_dim).into_iter().map(
+            |_| normal.sample_iter(&mut thread_rng()).take(feature_dim).collect::<Vec<f32>>()
+        ).collect();
+
+        let context_bias: Vec<f32> = normal.sample_iter(&mut thread_rng()).take(context_dim).collect();
+
+        HalfSpaceContext {
+            feature_dim,
+            context_dim,
+            context_maps,
+            context_bias,
+        }
+    }
+}
 
 impl ContextFunction for HalfSpaceContext {
-    fn indicator_func(&self, side_effects: &Vec<f32>) -> Vec<bool> {
-        todo!()
+    fn indicator_func(&self, side_info: &[f32]) -> Vec<bool> {
+        let mut results = Vec::with_capacity(self.context_dim);
+        // split space by x . v  > b
+        for row_index in 0..self.context_dim {
+            let mut value = 0.0;
+            for col_index in 0..self.feature_dim {
+                value += self.context_maps[row_index][col_index] * side_info[col_index];
+            }
+            results.push(value > self.context_bias[row_index]);
+        }
+        results
     }
 }
 
 pub struct SkipGramContext {}
 
 impl ContextFunction for SkipGramContext {
-    fn indicator_func(&self, side_effects: &Vec<f32>) -> Vec<bool> {
+    fn indicator_func(&self, side_info: &[f32]) -> Vec<bool> {
         todo!()
     }
+}
+
+#[test]
+fn test_new_half_space_context() {
+    // このテストは後々いらない。初期化がうまく出来ているかチェックするためだけ。
+    let actual = HalfSpaceContext::new(2, 3);
+}
+
+#[test]
+fn test_half_space_indicator_func() {
+    let side_info = vec![1.2, 1.5, 0.9];
+    let context_dim = 4;
+    let feature_dim = 3;
+    let half_space_context = HalfSpaceContext::new(context_dim, feature_dim);
+    // TODO: テストできないので、乱数の生成はシードを固定できるようにする。
+    let actual = half_space_context.indicator_func(&side_info);
 }
--- src/model/gate.rs
+++ src/model/gate.rs
@@ -1,24 +1,25 @@
-use mockall::mock;
+use mockall::{CaseTreeExt, mock};
 
 use crate::model::context_func::ContextFunction;
 use crate::model::context_func::HalfSpaceContext;
 
-pub struct Gate<'a> {
+pub struct Gate<C: ContextFunction> {
     input_dim: usize,
     weights: Vec<Vec<f32>>,
-    context_func: &'a dyn ContextFunction,
+    context_func: C,
 }
 
-impl<'a> Gate<'a> {
+impl<C: ContextFunction> Gate<C> {
     pub fn with_half_space_context<F>(input_dim: usize,
                                       context_dim: usize,
-                                      weight_init_func: F) -> Self
+                                      feature_dim: usize,
+                                      weight_init_func: F) -> Gate<HalfSpaceContext>
         where F: Fn(usize, usize) -> Vec<Vec<f32>>
     {
         Gate {
             input_dim: input_dim,
             weights: weight_init_func(input_dim, context_dim),
-            context_func: &HalfSpaceContext {},
+            context_func: HalfSpaceContext::new(context_dim, feature_dim),
         }
     }
 
@@ -29,8 +30,14 @@ impl<'a> Gate<'a> {
         &self.weights[indicator]
     }
 
+    pub fn update_weights(&mut self, context_index: usize, weights: Vec<f32>) {
+        self.weights[context_index] = weights;
+    }
+
     fn transform_contexts_to_weight_indicator(contexts: Vec<bool>) -> usize {
         let mut weight_indicator: i32 = 0;
+        // Transform a binary number context to a decimal number.
+        // This decimal number specifies the index of the weight to be used.
         for (index, bit) in contexts.iter().enumerate() {
             weight_indicator += 2_i32.pow(index as u32) * (*bit as i32)
         }
@@ -41,14 +48,14 @@ impl<'a> Gate<'a> {
 fn initialize_balanced_weights(input_dim: usize, context_dim: usize) -> Vec<Vec<f32>> {
     let init_value: f32 = 1.0 / (input_dim as f32);
     (0..context_dim).into_iter().map(
-        |ignore| (0..input_dim).into_iter().map(|ignore| init_value).collect::<Vec<f32>>()
+        |_| (0..input_dim).into_iter().map(|_| init_value).collect::<Vec<f32>>()
     ).collect()
 }
 
 #[test]
 fn test_transform_bits_to_weight_indicator() {
     let contexts = vec![true, false, true, true];
-    let actual = Gate::transform_contexts_to_weight_indicator(contexts);
+    let actual = Gate::<HalfSpaceContext>::transform_contexts_to_weight_indicator(contexts);
     assert_eq!(actual, 13);
 }
 
@@ -56,7 +63,7 @@ mock! {
     pub ContextFunctionM {}
 
     impl ContextFunction for ContextFunctionM {
-        fn indicator_func(&self, side_effects: &Vec<f32>) -> Vec<bool>;
+        fn indicator_func(&self, side_effects: &[f32]) -> Vec<bool>;
     }
 }
 
@@ -68,13 +75,22 @@ fn test_select_weights() {
     let gate = Gate {
         input_dim: 4,
         weights: vec![vec![0.1, 0.2], vec![0.3, 0.4], vec![0.5, 0.6], vec![0.7, 0.8]],
-        context_func: &mock_context_func,
+        context_func: mock_context_func,
     };
 
     let side_effects = vec![0.1, 0.2, 0.2, 0.9];
     let actual = gate.select_weights(&side_effects);
 
     assert_eq!(*actual, vec![0.5, 0.6]);
+}
+
+#[test]
+fn test_update_weights() {
+    let mut gate = Gate::<HalfSpaceContext>::with_half_space_context(2, 3, 10, initialize_balanced_weights);
+    gate.update_weights(0, vec![0.2, 0.1]);
+    let actual = &gate.weights[0];
+    let expected: Vec<f32> = vec![0.2, 0.1];
+    assert_eq!(*actual, expected);
 }
 
 #[test]
