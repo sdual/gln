9008db06237f7a6986b86b6efffb42aa68ad2bcc
sdual <shr.nrht.0623@gmail.com>
2022-10-21 02:58:22 +0900

    fix gln initialization

--- src/model/gln.rs
+++ src/model/gln.rs
@@ -8,11 +8,11 @@ struct GLN {
 
 impl GLN {
     pub fn new(neuron_nums: Vec<i32>, context_dim: usize, feature_dim: usize) -> Self {
-        let num_layers = neuron_nums.len();
         let mut layers = Vec::with_capacity(neuron_nums.len());
         let first_layer = Layer::with_neuron_num(neuron_nums[0], feature_dim, context_dim, feature_dim);
         layers.push(first_layer);
-        // TODO: スライスを使う
+
+        let num_layers = neuron_nums.len();
         for layer_index in (0..num_layers).skip(1) {
             let input_dim = neuron_nums[layer_index - 1] as usize;
             let layer = Layer::with_neuron_num(neuron_nums[layer_index], input_dim, context_dim, feature_dim);
